df7636766db6dabfd770eccd743f422e2d828913
ilovepc <ilovepc_@naver.com>
2020-07-08 04:26:36 +0900

    pug 설치 후 응답을 pug로 보내기 (res.render사용)

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express()
-
+app.set('view engine',"pug");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(helmet()); //그냥 보안을 위한것.
-
+//라우터
 app.use(routes.home,globalRouter); //join, /search, /home, / 등등을 다룸.
 app.use(routes.users,userRouter); //누군가 /user 경로로 접근하면 이 router 전체를 사용하겠다는 의미
 app.use(routes.videos,videoRouter);
