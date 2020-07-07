c89240dc816d6a8f9cba46fb82c3a051bea35ac7
ilovepc <ilovepc_@naver.com>
2020-07-08 03:54:43 +0900

    #2.11 controller 생성완료. 결국 controller란 해당 경로에 요청이 왔을 때 요청을 받아들이고 응답하는 역할인거같다.

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import routes from "../routes";
+import { users, editProfile, changePassword, userDetail } from "../controllers/userController";
 const userRouter = express.Router();
-userRouter.get(routes.users,(req,res)=>res.send('users'));
-userRouter.get(routes.editProfile,(req,res)=>res.send('editProfile'));
-userRouter.get(routes.changePassword,(req,res)=>res.send('changePassword'));
-userRouter.get(routes.userDetail,(req,res)=>{res.send('userDetail'); console.log('dd');}); //와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
+userRouter.get(routes.users,users);
+userRouter.get(routes.editProfile,editProfile);
+userRouter.get(routes.changePassword,changePassword);
+userRouter.get(routes.userDetail,userDetail); //와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
+import { videos, upload, editVideo, deleteVideo, videoDetail } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos,(req,res)=>res.send('VIDEOS'));
-videoRouter.get(routes.upload,(req,res)=>res.send('upload'));
-videoRouter.get(routes.editVideo,(req,res)=>res.send('editVideo'));
-videoRouter.get(routes.deleteVideo,(req,res)=>res.send('deleteVideo'));
-videoRouter.get(routes.videoDetail,(req,res)=>res.send('videoDetail'));
+videoRouter.get(routes.videos,videos);
+videoRouter.get(routes.upload,upload);
+videoRouter.get(routes.editVideo,editVideo);//와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
+videoRouter.get(routes.deleteVideo,deleteVideo);
+videoRouter.get(routes.videoDetail,videoDetail);
 
 export default videoRouter;
