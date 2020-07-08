51c2ff05ffc5791010e2c9392f71065d050c2759
ilovepc <ilovepc_@naver.com>
2020-07-09 03:29:55 +0900

    Fake login(locals에 user정보를 담아서 전역변수처럼 사용), id가 들어가는 URL수정(routes.js)

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
 import routes from "../routes";
-import { users, editProfile, changePassword, userDetail } from "../controllers/userController";
+import { editProfile, changePassword, userDetail } from "../controllers/userController";
 const userRouter = express.Router();
 userRouter.get(routes.editProfile,editProfile);
 userRouter.get(routes.changePassword,changePassword);
-userRouter.get(routes.userDetail,userDetail); //와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
+userRouter.get(routes.userDetail(),userDetail); //와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
 export default userRouter;
