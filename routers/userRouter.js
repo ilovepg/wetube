b4b2dc00fdef9d6f8a5ce3f08017d019bac59aba
ilovepc <ilovepc_@naver.com>
2020-07-08 22:06:47 +0900

    template변수 사용법 적용

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 import routes from "../routes";
 import { users, editProfile, changePassword, userDetail } from "../controllers/userController";
 const userRouter = express.Router();
-userRouter.get(routes.users,users);
 userRouter.get(routes.editProfile,editProfile);
 userRouter.get(routes.changePassword,changePassword);
 userRouter.get(routes.userDetail,userDetail); //와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,7 +3,6 @@ import routes from "../routes";
 import { videos, upload, editVideo, deleteVideo, videoDetail } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos,videos);
 videoRouter.get(routes.upload,upload);
 videoRouter.get(routes.editVideo,editVideo);//와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
 videoRouter.get(routes.deleteVideo,deleteVideo);
