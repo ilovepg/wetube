362f8e85c6a146a47e142124fa0aab6789a9118a
ilovepc <ilovepc_@naver.com>
2020-07-08 18:45:49 +0900

    locals를 사용하여 로컬변수를 전역적으로 템플릿에서 사용하기

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 //import {userRouter} from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express()
+app.use(helmet()); //그냥 보안을 위한것.
 app.set("view engine","pug"); //view engine을 pug로 설정
-
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-app.use(helmet()); //그냥 보안을 위한것.
+
+//local변수를 global변수로 사용하도록 만들어주는 것이라고 생각
+app.use(localsMiddleware);
 //라우터
 app.use(routes.home,globalRouter); //join, /search, /home, / 등등을 다룸.
 app.use(routes.users,userRouter); //누군가 /user 경로로 접근하면 이 router 전체를 사용하겠다는 의미
