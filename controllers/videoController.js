e5117745375254fe083ebc1d3f0cfbff83fcd59c
ilovepc <ilovepc_@naver.com>
2020-07-09 01:18:22 +0900

    HOME video dummy data 삽입

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,7 @@
-export const home = (req,res) => res.render("home",{pageTitle: "Home"});
+import {videos} from "../db"
+export const home = (req,res) => {
+    res.render("home",{pageTitle: "Home",videos});
+};
 export const search = (req,res) => {
     //const searchingBy=req.query.term; //ES6 이전방식에는 이렇게 사용했다.
     const { //ES6 문법 : req.query.term을 한것과 같다.
@@ -6,7 +9,7 @@ export const search = (req,res) => {
     } = req; 
     
     res.render('search',{pageTitle:"Search",searchingBy});
-}
+};
 export const upload = (req,res) => res.render('upload',{pageTitle:"Upload"});
 export const videoDetail = (req,res) => res.render('videoDetail',{pageTitle:"Video Detail"});
 export const editVideo = (req,res) => res.render('editVideo',{pageTitle:"Edit Video"});
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324234,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+        creator: {
+            id:123213,
+            name:"ilovepc",
+            email:"ilovepc_@naver.com"
+        }
+    },
+    {
+        id:323232,
+        title: 'Video Super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+        creator: {
+            id:123213,
+            name:"ilovepc",
+            email:"ilovepc_@naver.com"
+        }
+    },
+    {
+        id:213213213,
+        title: 'Video Nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+        creator: {
+            id:123213,
+            name:"ilovepc",
+            email:"ilovepc_@naver.com"
+        }
+    },
+    {
+        id:32323,
+        title: 'Video Perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+        creator: {
+            id:123213,
+            name:"ilovepc",
+            email:"ilovepc_@naver.com"
+        }
+    }
+]
