ba806c6495ae3aa30f0b0288ad23c02fe64c5fea
ilovepc <ilovepc_@naver.com>
2020-07-10 00:10:37 +0900

    #3.6 video upload 좀 헷갈려서 여기서 중간정리 해야함.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,23 +1,36 @@
-import routes from "../routes"
+import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req,res) => {
-    res.render("home",{pageTitle: "Home",videos});
+export const home = async (req,res) => {
+    try{
+        const videos = await Video.find({}); //DB에 있는 모든 VIDEO 가져오기
+        res.render("home",{pageTitle: "Home",videos});
+    }catch(error){
+        console.log(error);
+        res.render("home",{pageTitle: "Home",videos:[]});
+    }
 };
 export const search = (req,res) => {
     //const searchingBy=req.query.term; //ES6 이전방식에는 이렇게 사용했다.
     const { //ES6 문법 : req.query.term을 한것과 같다.
         query:{term:searchingBy}
     } = req; 
-    
     res.render('search',{pageTitle:"Search",searchingBy,videos});
 };
 export const getUpload = (req,res) => res.render('upload',{pageTitle:"Upload"});
-export const postUpload = (req,res) => {
+export const postUpload = async (req,res) => {
     const {
-        body:{file,title,description}
+        body:{title,description},
+        file:{path}
     }=req;
+    const newVideo = await Video.create({
+        fileUrl : path,
+        title,
+        description
+    });
+    console.log(newVideo);
     //To Do : Upload and save video
-    res.redirect(routes.videoDetail(324234));
+    res.redirect(routes.videoDetail(newVideo.id));
 }
 
 export const videoDetail = (req,res) => res.render('videoDetail',{pageTitle:"Video Detail"});
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import routes from "../routes";
 import {editVideo, deleteVideo, videoDetail, getUpload, postUpload } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload,getUpload);
-videoRouter.post(routes.upload,postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.editVideo,editVideo);//와일드카드 라우터는 항상 뒤에 적어주는 것이 좋다.
 videoRouter.get(routes.deleteVideo,deleteVideo);
 videoRouter.get(routes.videoDetail(),videoDetail);
